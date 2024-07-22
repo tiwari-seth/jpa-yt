3806428a85026894b999ed4bddb03b01abe94ed4
hp <ayush@>
2024-07-22 17:45:26 +0530

    1st

---
+++ src/main/java/com/example/jpayt/JpaYtApplication.java
@@ -0,0 +1,12 @@
+package com.example.jpayt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class JpaYtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaYtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpayt/JpaYtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpayt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaYtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
